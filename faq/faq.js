501705aae3131f2e9e7864e84e5104e499e477cd
tiramipseudonym <45605154+tiramipseudonym@users.noreply.github.com>
2023-04-17 21:18:42 -0400

    new garage coding

---
+++ faq/faq.js
@@ -0,0 +1,8 @@
+		function toggleAnswer(id) {
+			var answer = document.getElementById(id);
+			if (answer.style.display === "none") {
+				answer.style.display = "block";
+			} else {
+				answer.style.display = "none";
+			}
+		}
